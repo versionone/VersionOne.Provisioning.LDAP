eda8a08f5bc6afada9413deddde6c7cd0fa4acb9
LCunningham <LCunningham@542498e7-56f8-f544-a0f2-a85913ac812a>
2010-01-24 01:38:51 +0000

    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/VersionOne.Provisioning@34565 542498e7-56f8-f544-a0f2-a85913ac812a

---
+++ VersionOne.Provisioning.LDAP.Tests/LDAPTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Configuration;
+
+namespace VersionOne.Provisioning.LDAP.Tests
+{
+    [TestFixture]
+    public class LDAPTests
+    {
+        private string _ldapServerPath;
+        private string _ldapGroupDN;
+        private string _ldapUsername;
+        private string _ldapPassword;
+        
+        [SetUp]      
+        public void SetUp()
+        {
+            _ldapServerPath = ConfigurationManager.AppSettings["ldapServerPath"];
+            _ldapGroupDN = ConfigurationManager.AppSettings["ldapGroupDN"];
+            _ldapUsername = ConfigurationManager.AppSettings["ldapUsername"];
+            _ldapPassword = ConfigurationManager.AppSettings["ldapPassword"];
+
+        }
+        
+        [Test]
+        public void TestGetUsersFromLdap()
+        {
+            LDAPReader ldapReader = new LDAPReader();
+            IList<LDAPUser> ldapUsers = new List<LDAPUser>();
+
+          //  ldapUsers = ldapReader.GetUsersFromLdap("192.168.36.4", "OU=Sales,OU=Users V1,DC=corp,DC=versionone,DC=net","","");
+
+            ldapUsers = ldapReader.GetUsersFromLdap(_ldapServerPath,_ldapGroupDN,_ldapUsername,_ldapPassword);
+             
+            
+            Assert.AreEqual(28,ldapUsers.Count);
+
+            
+          
+        }
+
+    }
+}
