c2a250056699501297d07715f020dec6d42cd9cb
LCunningham <LCunningham@542498e7-56f8-f544-a0f2-a85913ac812a>
2010-01-24 01:37:44 +0000

    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/VersionOne.Provisioning@34563 542498e7-56f8-f544-a0f2-a85913ac812a

---
+++ VersionOne.Provisioning.Logging/Logger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+namespace VersionOne.Provisioning.Logging
+{
+    public class Logger
+    {
+        private string _logPath;
+
+        public Logger(string logPath)
+        {
+            this._logPath = logPath;
+        }
+
+        public void LogToTextFile(StringBuilder message)
+        {
+            string textToWrite = message.ToString();
+            string path = _logPath; 
+            FileStream fstream = new FileStream(path, FileMode.Append, FileAccess.Write);
+            StreamWriter swriter = new StreamWriter(fstream);
+
+            swriter.Write(textToWrite);
+            message.Remove(0, message.Length);
+            swriter.Close();
+        }
+    }
+}
